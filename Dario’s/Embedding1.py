710da4371bd2d813a3faf7578179fb83b68cb2ab
DarioMaddaloni <105283767+DarioMaddaloni@users.noreply.github.com>
2022-10-19 09:43:36 +0200

    Update Embedding1.py
    Very useless embedding and detection. I wrote this oltre to have something to try

--- Dario’s/Embedding1.py
+++ Dario’s/Embedding1.py
@@ -1,6 +1,25 @@
+def wpsnr(img1, img2):
+    img1 = np.float32(img1) / 255.0
+    img2 = np.float32(img2) / 255.0
+
+    difference = img1 - img2
+    same = not np.any(difference)
+    if same is True:
+        return 9999999
+    csf = np.genfromtxt('csf.csv', delimiter=',')
+    ew = convolve2d(difference, np.rot90(csf, 2), mode='valid')
+    decibels = 20.0 * np.log10(1.0 / sqrt(np.mean(np.mean(ew ** 2))))
+    return decibels
+
 def embedding(image):
+  image[0,0]=0
   return image
 
-
-def detection(image):
-  return 1;
+#1=watermarkPresent, 2=noWatermark
+def detection(originalImage, watermarkedImage, attackedImage):
+  wpsnrValue=wpsnr(watermarkedImage, attackedImage)
+  if (watermarkedImage-attackedImage).all()==0:
+     present=1
+  else:
+    present=0
+  return present, wpsnrValue
