c10dceb1ae6607ca013cb2f6d83c4b204a3a85ea
Giacomo Borin <d.borin68.gb@gmail.com>
2022-10-20 10:51:02 +0200

    working_embedding

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import embedding as em
 import detection as dt
 import attack as at
-
+from embedding import embedding
+from detection import extraction,detection
+from psnr import similarity, wpsnr
 import cv2
 
 
@@ -17,7 +19,26 @@ def main():
     # embed watermark into three different pictures of 512x512 (as it will be in the challenge)
     pictures = ['watermarking-images/lena.bmp', 'watermarking-images/baboon.bmp', 'watermarking-images/cameraman.tif']
     watermarked_pictures = []
+    name_image = 'lena.bmp'
+    name_out = 'wat_lena.bmp'
+    watermarked = embedding(name_image, mark, alpha, name_output=name_out)
+    mark_ex = extraction(image = cv2.imread(name_image, 0), watermarked=watermarked, mark_size=mark.size,alpha=alpha)
+    #print(mark_ex)
+    sim = (similarity(mark_ex,mark))
+    from psnr import compute_thr
+    T = compute_thr(sim,mark_size, w = mark)
+    print(T)
 
+    atk = at.random_attack(watermarked)
+    name_atk = 'atk_lena.bmp'
+    cv2.imwrite(name_atk, atk)
+
+    print(detection(name_image,name_out,name_atk,mark,T,alpha))
+
+
+    #out1, w = detection(name_image,
+
+    """
     for img_path in pictures:
         # embed mark in picture
         picture = cv2.imread(img_path, 0)
@@ -29,7 +50,7 @@ def main():
 
         # use detection to see whether attack was successful and mark was removed
         dt.detection(attacked, watermarked, alpha, mark_size, v)
-
+    """
     # roccurve.compute_roc(alpha, mark_size, v)
 
 
