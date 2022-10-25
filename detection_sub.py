964597c76d763e8d4f5d098caf7cc62a5959a081
Giacomo Borin <d.borin68.gb@gmail.com>
2022-10-25 22:00:11 +0200

    cleared the code

--- detection_sub.py
+++ detection_sub.py
@@ -11,24 +11,6 @@ def im_idct(image):
     return (idct(idct(image, axis=1, norm='ortho'), axis=0, norm='ortho'))
 
 
-def extraction_DCT(image, watermarked, mark_size, alpha, v='multiplicative'):
-    ori_dct = dct(dct(image,axis=0, norm='ortho'),axis=1, norm='ortho')
-    wat_dct = dct(dct(watermarked,axis=0, norm='ortho'),axis=1, norm='ortho')
-    # Get the locations of the most perceptually significant components
-    ori_dct = abs(ori_dct)
-    wat_dct = abs(wat_dct)
-    locations = np.argsort(-ori_dct,axis=None) # - sign is used to get descending order
-    rows = image.shape[0]
-    locations = [(val//rows, val%rows) for val in locations] # locations as (x,y) coordinates
-    # Generate a watermark
-    w_ex = np.zeros(mark_size, dtype=np.float64)
-    # Embed the watermark
-    for idx, loc in enumerate(locations[1:mark_size+1]):
-        if v=='additive':
-            w_ex[idx] =  (wat_dct[loc] - ori_dct[loc]) /alpha
-        elif v=='multiplicative':
-            w_ex[idx] =  (wat_dct[loc] - ori_dct[loc]) / (alpha*ori_dct[loc])
-    return w_ex
 
 import cv2, pywt
 
@@ -70,9 +52,6 @@ def extraction(image, watermarked, mark_size, alpha, dim = 8, step = 15):
 
     for i in range(0, sh[0], dim):
         for j in range(0, sh[1], dim):
-            #if q[i//dim,j//dim] == 0:
-                # mark.append(extraction_DCT(image[i:i + dim - 1, j:j + dim - 1], watermarked[i:i + dim - 1, j:j + dim - 1],
-                #                           mark_size = sub_mark_size,  alpha=alpha))
             if q[i // dim, j // dim] != 0:
                 mark.append((extraction_SVD(im_dct(image[i:i + dim - 1, j:j + dim - 1]), im_dct(watermarked[i:i + dim - 1, j:j + dim - 1])
                                                                       ,mark_size=sub_mark_size,  alpha=(q[i // dim, j // dim]) * alpha, mode = "d")))
@@ -97,40 +76,3 @@ def detection(name_original, name_watermarked, name_attacked, mark_size=1024,  t
     else:
         out1 = 0
     return out1, wpsnr(wat_original,wat_attacked)
-
-
-"""
-Computes the similarity measure between the original and the new watermarks.
-"""
-
-def compute_thr(sim, mark_size, w):
-    SIM = np.zeros(1000)
-    SIM[0] = abs(sim)
-    for i in range(1, 1000):
-        r = np.random.uniform(0.0, 1.0, mark_size)
-        SIM[i] = abs(similarity(w, r))
-
-    SIM.sort()
-    t = SIM[-2]
-    T = t + (0.1 * t)
-    print(T)
-    return T
-
-
-"""
-Computes the similarity measure between the original and the new watermarks.
-"""
-
-"""
-This function computes the decision whether mark was destroyed or not.
-"""
-
-
-def decision(mark, mark_size, w_ex):
-    sim = similarity(mark, w_ex)
-    threshold = compute_thr(sim, mark_size, mark)
-
-    if sim > threshold:
-        print('Mark has been found. SIM = %f' % sim)
-    else:
-        print('Mark has been lost. SIM = %f' % sim)
--- embedding_sub.py
+++ embedding_sub.py
@@ -26,43 +26,12 @@ def im_idct(image):
 
 # here we will insert differnt possibilities for embedding
 
-def embedding_DCT(image, mark, alpha = 0.1, v='multiplicative', plot_mark = False):
-    # Get the DCT transform of the image
-    ori_dct = dct(dct(image,axis=0, norm='ortho'),axis=1, norm='ortho')
-    # Get the locations of the most perceptually significant components
-    sign = np.sign(ori_dct)
-    ori_dct = abs(ori_dct)
-    locations = np.argsort(-ori_dct,axis=None) # - sign is used to get descending order
-    rows = image.shape[0]
-    locations = [(val//rows, val%rows) for val in locations] # locations as (x,y) coordinates
-    # Embed the watermark
-    watermarked_dct = ori_dct.copy()
-    mark_places = ori_dct.copy()
-    mark_places[:,:] = 1
-    for idx, (loc,mark_val) in enumerate(zip(locations[1:], mark)):
-        if v == 'additive':
-            watermarked_dct[loc] += (alpha * mark_val)
-        elif v == 'multiplicative':
-            watermarked_dct[loc] *= 1 + ( alpha * mark_val)
-            mark_places[loc] = 0
-    # Restore sign and o back to spatial domain
-    if plot_mark:
-        plt.figure(figsize=(15, 6))
-        plt.title('Position for watermark embedding')
-        plt.imshow(mark_places, cmap='gray')
-        plt.show()
-    watermarked_dct *= sign
-    watermarked = np.uint8(idct(idct(watermarked_dct,axis=1, norm='ortho'),axis=0, norm='ortho'))
-    return watermarked
-
 
 def embedding_SVD(image,mark, alpha = 1, mode = 'additive'):
     u, s, v = np.linalg.svd(image)
     if mark.size >= s.size :
         print('error mark',mark.size,'diag',s.size)
         return 123
-
-
     if mode == 'multiplicative':
         mark_pad = np.pad(mark, (1, s.size - mark.size - 1), 'constant')
         s *= 1 + alpha * mark_pad
@@ -84,7 +53,6 @@ import pywt
 def embedding(name_image, mark, alpha = 10, name_output = 'watermarked.bmp', dim = 8 , step = 15):
     # first level
     image = cv2.imread(name_image, 0)
-
     q = hvs.hvs_step(image, dim = dim, step = step)
 
     coeffs2 = pywt.dwt2(image, 'haar')
@@ -96,35 +64,16 @@ def embedding(name_image, mark, alpha = 10, name_output = 'watermarked.bmp', dim
     if sh[0] % dim != 0:
         return 'img size not div by ' + str(dim)
 
-    #if mark.size % ((sh[0]//dim)*(sh[1]//dim)) != 0:
-    #    return 'mark size not div by ' + str(dim)
-
-    # watermarked = image.copy()
     sub_mark = np.array_split(mark, np.count_nonzero(q))
     sub_mark_size = sub_mark[0].size
     for i in range(len(sub_mark)):
         sub_mark[i] = np.pad(sub_mark[i], (0,sub_mark_size - sub_mark[i].size), 'constant')
     for i in range(0,sh[0],dim):
         for j in range(0,sh[1],dim):
-            #if q[i//dim,j//dim] == 0 :
-            #   image[i:i + dim - 1, j:j + dim - 1] = embedding_DCT((image[i:i + dim - 1, j:j + dim - 1]),
-            #                                                               sub_mark.pop(),alpha=alpha)
             if q[i // dim, j // dim] != 0:
                 image[i:i+dim-1,j:j+dim-1] = im_idct(embedding_SVD(im_dct(image[i:i+dim-1,j:j+dim-1]),
                                                              sub_mark.pop(0), alpha = (q[i // dim, j // dim])*alpha , mode = "d"))
 
-
-
-    # second level
-    coeffs2_2 = pywt.dwt2(image, 'haar')
-    LL2, (LH2, HL2, HH2) = coeffs2_2
-
-    # third level
-    LL3, (LH3, HL3, HH3) = pywt.dwt2(LL2, 'haar')
-
-    watermarked =  pywt.idwt2((image, (LH, HL, HH)), 'haar')
-    # watermarked =  pywt.idwt2((pywt.idwt2((LL2, (LH2, HL2, HH2)), 'haar'), (LH, HL, HH)), 'haar')
-    # return pywt.idwt2((pywt.idwt2((pywt.idwt2((LL3, (LH3, HL3, HH3)), (LH2, HL2, HH2)), 'haar'), (LH, HL, HH)), 'haar')
-
+    watermarked = pywt.idwt2((image, (LH, HL, HH)), 'haar')
     cv2.imwrite(name_output, watermarked)
     return watermarked
