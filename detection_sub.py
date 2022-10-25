6bcfff6747afc2413e3acc29f49f2bed5339fc55
Giacomo Borin <d.borin68.gb@gmail.com>
2022-10-25 17:07:37 +0200

    step

--- detection_sub.py
+++ detection_sub.py
@@ -48,12 +48,12 @@ def extraction_SVD(image, watermarked, alpha, mark_size, mode='additive'):
             w_ex[i] = (s_wat[locations[i+1]] - s[locations[i+1]])/alpha
         return w_ex
 
-def extraction(image, watermarked, mark_size, alpha, dim = 8):
+def extraction(image, watermarked, mark_size, alpha, dim = 8, step = 15):
     # extraction phase
     # first level
     mark = []
 
-    q = hvs.hvs_step(image, dim = dim, step = 15)
+    q = hvs.hvs_step(image, dim = dim, step = step)
 
     # SUB LEVEL EMBEDDING
 
@@ -61,8 +61,13 @@ def extraction(image, watermarked, mark_size, alpha, dim = 8):
     image, (LH_ori, HL_ori, HH_ori) = pywt.dwt2(image, 'haar')
     sh = image.shape
     watermarked, (LH_wat, HL_wat, HH_wat) = pywt.dwt2(watermarked, 'haar')
-    sub_mark_size = mark_size // np.count_nonzero(q) + 1
-    last_mark_size = mark_size % sub_mark_size + 1
+
+    if mark_size % np.count_nonzero(q) == 0 :
+        sub_mark_size = mark_size // np.count_nonzero(q)
+    else:
+        sub_mark_size = mark_size // np.count_nonzero(q) + 1
+        last_mark_size = sub_mark_size - 1
+
     for i in range(0, sh[0], dim):
         for j in range(0, sh[1], dim):
             #if q[i//dim,j//dim] == 0:
@@ -72,13 +77,14 @@ def extraction(image, watermarked, mark_size, alpha, dim = 8):
                 mark.append((extraction_SVD(im_dct(image[i:i + dim - 1, j:j + dim - 1]), im_dct(watermarked[i:i + dim - 1, j:j + dim - 1])
                                                                       ,mark_size=sub_mark_size,  alpha=(q[i // dim, j // dim]) * alpha, mode = "d")))
 
-    for i in range( mark_size % np.count_nonzero(q), len(mark)):
-        mark[i] = mark[i][:last_mark_size]
+    if mark_size % np.count_nonzero(q) != 0:
+        for i in range( mark_size % np.count_nonzero(q), len(mark)):
+            mark[i] = mark[i][:last_mark_size]
 
     mark = np.concatenate(mark)
     return mark
 
-def detection(name_original, name_watermarked, name_attacked,mark_size=1024,  threeshold = 2, alpha = 10):
+def detection(name_original, name_watermarked, name_attacked, mark_size=1024,  threeshold = 2, alpha = 10):
     image = cv2.imread(name_original, 0)
     wat_original = cv2.imread(name_watermarked,0)
     wat_attacked = cv2.imread(name_attacked,0)
