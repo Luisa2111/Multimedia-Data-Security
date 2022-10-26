651d976dd34f1061e5b619a51de71fd45c462908
Giacomo Borin <d.borin68.gb@gmail.com>
2022-10-26 13:53:43 +0200

    Update embedding_flat_file.py

--- embedding_flat_file.py
+++ embedding_flat_file.py
@@ -1,6 +1,14 @@
 import numpy as np
+from scipy.fft import dct, idct
+
+def im_dct(image):
+    return dct(dct(image,axis=0, norm='ortho'),axis=1, norm='ortho')
+
+def im_idct(image):
+    return (idct(idct(image, axis=1, norm='ortho'), axis=0, norm='ortho'))
 
 def embedding_flat(block, wat):
+    block = im_dct(block)
     if not hasattr(wat, "__len__"):
         if wat==1:
             if block[0,1]<=0:
@@ -20,3 +28,4 @@ def embedding_flat(block, wat):
             print("the wat value must be -1 or 1. Not", wat)
     else:
         print("wat must be an integer not a list")
+    return im_idct(block)
