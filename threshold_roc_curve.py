5bf81123ada021e714a92b78989280787890e28b
Giacomo Borin <d.borin68.gb@gmail.com>
2022-10-27 15:42:39 +0200

    roc last calc

--- threshold_roc_curve.py
+++ threshold_roc_curve.py
@@ -43,7 +43,7 @@ def compute_roc(mark_size, alpha, mark, dim, step,
                               )
         # code added to complain with the fact that we should not use the mark
         sample = 0
-        while sample < 5:
+        while sample < 16:
             # fakemark = np.random.standard_normal(mark_size)
             fakemark = dt.extraction(image, cv2.imread('fakemarks/wat_' + files[i].rsplit( ".", 1 )[ 0 ] + '-' + str(sample).zfill(2) + '.bmp',0),
                                      mark_size, alpha=alpha,
@@ -88,7 +88,7 @@ def compute_roc(mark_size, alpha, mark, dim, step,
     # f = open("roc_out/roc_curve_%s%s%s-%s%s.txt" % (e.year, e.month, e.day, e.hour, e.minute), "a")
     # f.close()
     sys.stdout = open("roc_out/roc_curve_%s%s%s-%s%s.txt" % (e.year, e.month, e.day, e.hour, e.minute), "a")
-    print('roc curve evaluated on'+files_used+'files with mode sub_cap_flat1.\nParameters :')
+    print('roc curve evaluated on '+str(files_used)+' files with mode sub_cap_flat1.\nParameters :')
     print('alpha',alpha)
     print('dim',dim)
     print('step',step)
@@ -144,4 +144,4 @@ if __name__ == "__main__":
                 step=step, max_splits=max_splits,
                 min_splits=min_splits, sub_size=sub_size,
                 Xi_exp=Xi_exp, Lambda_exp=Lambda_exp, L_exp=L_exp, ceil = ceil,
-                files_used= 3)
+                files_used= 0)
