1c2c63f8533fb6b0906d5dd737cc6f406c020157
Giacomo Borin <d.borin68.gb@gmail.com>
2022-10-27 13:42:21 +0200

    new fakemarks

--- fakemarks.py
+++ fakemarks.py
@@ -2,6 +2,7 @@ import embedding
 import embeddingSS
 import cv2
 import numpy as np
+import attack as at
 
 if __name__ == "__main__":
     path_out = 'fakemarks/'
@@ -12,14 +13,31 @@ if __name__ == "__main__":
     for im in range(101):
         img = str(im).zfill(4)
         name_image = 'sample-images-roc/' + img + '.bmp'
-        name_out = path_out + 'wat_' + img + '-00' + '.bmp'
-        embeddingSS.embedding(name_image, mark, name_output=name_out,alpha=0.05)
-        name_out = path_out + 'wat_' + img + '-01' + '.bmp'
-        embeddingSS.embedding(name_image, mark, name_output=name_out)
-        name_out = path_out + 'wat_' + img + '-02' + '.bmp'
-        embedding.embedding(name_image, mark, name_output=name_out)
-        name_out = path_out + 'wat_' + img + '-03' + '.bmp'
-        embedding.embedding(name_image, mark, name_output=name_out, alpha= 0.5)
-        name_out = path_out + 'wat_' + img + '-04' + '.bmp'
-        embedding.embedding(name_image, mark, name_output=name_out, alpha = 1)
+        # name_out = path_out + 'wat_' + img + '-00' + '.bmp'
+        # embeddingSS.embedding(name_image, mark, name_output=name_out,alpha=0.05)
+        # name_out = path_out + 'wat_' + img + '-01' + '.bmp'
+        # embeddingSS.embedding(name_image, mark, name_output=name_out)
+        # name_out = path_out + 'wat_' + img + '-02' + '.bmp'
+        # embedding.embedding(name_image, mark, name_output=name_out)
+        # name_out = path_out + 'wat_' + img + '-03' + '.bmp'
+        # embedding.embedding(name_image, mark, name_output=name_out, alpha= 0.5)
+        # name_out = path_out + 'wat_' + img + '-04' + '.bmp'
+        # embedding.embedding(name_image, mark, name_output=name_out, alpha = 1)
+        name_out = path_out + 'wat_' + img + '-05' + '.bmp'
+        embeddingSS.embedding(name_image, mark, name_output=name_out, alpha=0.5)
+        name_out = path_out + 'wat_' + img + '-06' + '.bmp'
+        embeddingSS.embedding(name_image, mark, name_output=name_out, alpha= 1)
+        name_out = path_out + 'wat_' + img + '-07' + '.bmp'
+        embedding.embedding(name_image, mark, name_output=name_out, alpha= 0.2)
+        name_out = path_out + 'wat_' + img + '-08' + '.bmp'
+        embedding.embedding(name_image, mark, name_output=name_out, alpha=0.7)
+        name_out = path_out + 'wat_' + img + '-09' + '.bmp'
+        embedding.embedding(name_image, mark, name_output=name_out, alpha=1.4)
+
+        image = cv2.imread(name_image,0)
+        for i in range(1,7):
+            name_out = path_out + 'wat_' + img + '-' + str(i+9).zfill(2) + '.bmp'
+            cv2.imwrite(name_out,at.attack_num(image,i))
+
+
 
